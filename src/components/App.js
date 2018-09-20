5f6f26d0038cd03b6cfc8e00f1f38fad0a32f162
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-09-20 11:21:18 -0700

    added a header and footer to the app

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
+import Header from './app/Header';
+import Footer from './app/Footer';
+import styles from './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        Hello World!
+      <div className={styles.app}>
+        <Header/>
+        <main>
+          This is the main section.
+        </main>
+        <Footer/>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 // import { Provider } from 'react-redux';
 import App from './components/App';
 // import store from './store/store';
+import 'reset-css';
 
 
 ReactDOM.render(
