daa27955d2a9fb2dacc1158d5f4252d25ab85a2d
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-09-20 10:46:34 -0700

    messing with the store

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
+// import { Provider } from 'react-redux';
 import App from './components/App';
-import store from './store/store';
+// import store from './store/store';
 
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App/>
-  </Provider>,
+  <App/>,
   document.getElementById('root')
 );
